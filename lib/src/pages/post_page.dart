54ecf5264cb184ee82c96a1b8676dabfba6218cd
JoseBautista <jlbautistavelasquez@gmail.com>
2020-10-04 16:12:08 -0500

    cierra sesion

--- lib/src/pages/post_page.dart
+++ lib/src/pages/post_page.dart
@@ -3,10 +3,13 @@ import 'package:helpify/src/bloc/provider.dart';
 import 'package:helpify/src/bloc/publicaciones_firebase_bloc.dart';
 import 'package:helpify/src/models/ong_model.dart';
 import 'package:helpify/src/models/publicacion_model.dart';
+import 'package:helpify/src/shared_prefs/preferencias_usuario.dart';
 import 'package:helpify/src/widgets/cabecera_widget.dart';
 import 'package:line_awesome_icons/line_awesome_icons.dart';
 
 class PostPage extends StatelessWidget {
+  final prefs = new PreferenciasUsuario();
+
   @override
   Widget build(BuildContext context) {
     final Publicacion publicacionData = ModalRoute.of(context).settings.arguments;
@@ -83,16 +86,19 @@ class PostPage extends StatelessWidget {
   }
 
   Widget _crearButton(BuildContext context, Publicacion publicacionData) {
-    return RaisedButton(
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-        child: Text("Hacer donación"),
-      ),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
-      color: Color(0xFFFFDB15),
-      textColor: Colors.black,
-      onPressed: () =>
-          Navigator.pushNamed(context, "payment", arguments: publicacionData),
-    );
+    if(prefs.rol == 1){
+      return RaisedButton(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+          child: Text("Hacer donación"),
+        ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        color: Color(0xFFFFDB15),
+        textColor: Colors.black,
+        onPressed: () =>
+            Navigator.pushNamed(context, "payment", arguments: publicacionData),
+      );
+    }
+    return Container();
   }
 }
--- lib/src/pages/settings_page.dart
+++ lib/src/pages/settings_page.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:helpify/src/shared_prefs/preferencias_usuario.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class SettingsPage extends StatelessWidget {
+  final prefs = new PreferenciasUsuario();
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -28,11 +34,28 @@ class SettingsPage extends StatelessWidget {
           },
         ),
         Divider(),
+        ListTile(
+          title: Text("Cerrar Sesión"),
+          leading: Icon(Icons.close),
+          trailing: Icon(Icons.arrow_forward_ios),
+          onTap: () => {
+            _cerrarSesion(context)
+          },
+        ),
+        Divider(),
         Text(
           "Hecho con <3 en Flutter.",
           textAlign: TextAlign.center,
         ),
       ],
     );
+  }
+
+  _cerrarSesion(BuildContext context) async {
+    prefs.idLogin = "";
+    prefs.rol = 0;
+    await _auth.signOut();
+
+    Navigator.of(context).pushNamedAndRemoveUntil('choose_role', (Route<dynamic> route) => false);
   }
 }
